89fa2dfbccacf9bb2841d3f4411d700a9b97e047
Kharas81 <34625007+Kharas81@users.noreply.github.com>
2026-04-18 17:23:04 +0200

    Update loader.js

--- js/features/archive/loader.js
+++ js/features/archive/loader.js
@@ -3,6 +3,7 @@ import CONFIG from '../../core/config.js';
 import ApiFetch from '../../core/api-fetch.js';
 import ApiCardLookup from '../../core/api-card-lookup.js';
 import CatalogRepository from './catalog-repository.js';
+import ArchiveMerge from './archive-merge.js';
 
 export const ArchiveLoader = {
     normalizeSetKey(setKey = '') {
@@ -42,6 +43,22 @@ export const ArchiveLoader = {
         return null;
     },
 
+    annotateMasterCard(card = {}, setKey = '') {
+        const hasAdventureRefs = Utils.normalizeArray(card?.adventure_refs).length > 0;
+
+        return ArchiveMerge.annotateCard(card, {
+            sourceScope: hasAdventureRefs ? 'adventure_specific' : 'set_catalog',
+            sourceKey: this.normalizeSetKey(setKey)
+        });
+    },
+
+    annotateSupplementalCard(card = {}, catalogKey = '') {
+        return ArchiveMerge.annotateCard(card, {
+            sourceScope: 'central_catalog',
+            sourceKey: Utils.normalizeString(catalogKey)
+        });
+    },
+
     async fetchMasterCards(setKey = '') {
         const resolvedSetKey = this.normalizeSetKey(setKey);
         const entries = await this.getMasterEntries(resolvedSetKey);
@@ -51,7 +68,7 @@ export const ArchiveLoader = {
             try {
                 const card = await this.loadEntryDetail(entry, resolvedSetKey);
                 if (card) {
-                    loadedCards.push(card);
+                    loadedCards.push(this.annotateMasterCard(card, resolvedSetKey));
                 }
             } catch (error) {
                 console.warn(
@@ -65,51 +82,40 @@ export const ArchiveLoader = {
         return loadedCards;
     },
 
-    async fetchSupplementalCards() {
-        return await CatalogRepository.getCards('schergen');
-    },
+    async fetchSupplementalCards(setKey = '') {
+        const resolvedSetKey = this.normalizeSetKey(setKey);
+        const catalogKeys = CONFIG.getSupplementalCatalogKeysForSet?.(resolvedSetKey) || [];
 
-    mergeCardsById(cards = []) {
-        const map = new Map();
-
-        for (const card of Utils.normalizeArray(cards)) {
-            const id = Utils.normalizeString(card?.id);
-            if (!id) {
-                continue;
-            }
-
-            map.set(id, card);
+        if (!catalogKeys.length) {
+            return [];
         }
 
-        return Array.from(map.values());
-    },
+        const results = await Promise.all(
+            catalogKeys.map(async catalogKey => {
+                const cards = await CatalogRepository.getCards(catalogKey);
 
-    sortCards(cards = []) {
-        return [...Utils.normalizeArray(cards)].sort((a, b) => {
-            const categoryA = Utils.normalizeString(a?.card_category || a?.type);
-            const categoryB = Utils.normalizeString(b?.card_category || b?.type);
+                return Utils.normalizeArray(cards)
+                    .map(card => this.annotateSupplementalCard(card, catalogKey));
+            })
+        );
 
-            if (categoryA !== categoryB) {
-                return categoryA.localeCompare(categoryB, 'de');
-            }
-
-            const nameA = Utils.normalizeString(a?.name || a?.id);
-            const nameB = Utils.normalizeString(b?.name || b?.id);
-
-            return nameA.localeCompare(nameB, 'de');
-        });
+        return results.flat();
     },
 
     async fetchCardsForSet(setKey = '') {
-        const masterCards = await this.fetchMasterCards(setKey);
-        const supplementalCards = await this.fetchSupplementalCards();
+        const resolvedSetKey = this.normalizeSetKey(setKey);
 
-        return this.sortCards(
-            this.mergeCardsById([
-                ...masterCards,
-                ...supplementalCards
-            ])
-        );
+        const [masterCards, supplementalCards] = await Promise.all([
+            this.fetchMasterCards(resolvedSetKey),
+            this.fetchSupplementalCards(resolvedSetKey)
+        ]);
+
+        const mergedCards = ArchiveMerge.mergeCards([
+            ...masterCards,
+            ...supplementalCards
+        ]);
+
+        return ArchiveMerge.sortCards(mergedCards);
     }
 };
 
