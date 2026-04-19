b61bf970d8efec8f7212b84a56ea983c0146cd02
Kharas81 <34625007+Kharas81@users.noreply.github.com>
2026-04-19 10:44:44 +0200

    Update archive-merge.js

--- js/features/archive/archive-merge.js
+++ js/features/archive/archive-merge.js
@@ -1,122 +1,31 @@
 import Utils from '../../core/utils.js';
-import CONFIG from '../../core/config.js';
+import ArchiveScoring from './archive-scoring.js';
+import ArchivePreference from './archive-preference.js';
+import ArchiveSort from './archive-sort.js';
 
 export const ArchiveMerge = {
     inferSourceScope(card = {}) {
-        const explicitScope = Utils.normalizeString(card?.source_scope);
-        if (explicitScope) {
-            return explicitScope;
-        }
-
-        const adventureRefs = Utils.normalizeArray(card?.adventure_refs);
-        if (adventureRefs.length > 0) {
-            return 'adventure_specific';
-        }
-
-        if (Utils.normalizeString(card?.set?.id)) {
-            return 'set_catalog';
-        }
-
-        return 'unknown';
+        return ArchivePreference.inferSourceScope(card);
     },
 
     getPriority(card = {}) {
-        const scope = this.inferSourceScope(card);
-        return CONFIG.getArchiveMergePriority?.(scope) ?? 0;
+        return ArchivePreference.getPriority(card);
     },
 
     hasUsableImage(card = {}) {
-        return Utils.hasRealImage(
-            card?.images?.front,
-            card?.image
-        );
+        return ArchiveScoring.hasUsableImage(card);
     },
 
     getCompletenessScore(card = {}) {
-        let score = 0;
-
-        if (Utils.normalizeString(card?.name)) score += 5;
-        if (this.hasUsableImage(card)) score += 15;
-
-        score += Utils.normalizeArray(card?.tags).length;
-        score += Utils.normalizeArray(card?.keywords).length;
-        score += Utils.normalizeArray(card?.subtypes).length * 2;
-        score += Utils.normalizeArray(card?.adventure_refs).length * 2;
-        score += Utils.normalizeArray(card?.rules?.action_table).length * 4;
-
-        if (Utils.normalizeString(card?.rules?.passive)) score += 5;
-        if (Utils.normalizeString(card?.rules?.success)) score += 3;
-        if (Utils.normalizeString(card?.rules?.fail)) score += 3;
-        if (Utils.normalizeString(card?.rules?.draw_effect)) score += 3;
-        if (Utils.normalizeString(card?.rules?.flavor)) score += 2;
-        if (Utils.normalizeString(card?.notes)) score += 1;
-
-        const stats = card?.stats || {};
-        [
-            stats?.gp,
-            stats?.lp,
-            stats?.armor,
-            stats?.evasion,
-            stats?.actions,
-            stats?.start_value,
-            stats?.cost
-        ].forEach(value => {
-            if (value !== null && value !== undefined && value !== '') {
-                score += 2;
-            }
-        });
-
-        return score;
+        return ArchiveScoring.getCompletenessScore(card);
     },
 
     annotateCard(card = {}, options = {}) {
-        const sourceScope = Utils.normalizeString(
-            options?.sourceScope || card?.source_scope || this.inferSourceScope(card)
-        ) || 'unknown';
-
-        const sourceKey = Utils.normalizeString(
-            options?.sourceKey || card?.source_key || ''
-        );
-
-        return {
-            ...card,
-            source_scope: sourceScope,
-            source_key: sourceKey,
-            archive_priority: CONFIG.getArchiveMergePriority?.(sourceScope) ?? 0
-        };
+        return ArchivePreference.annotateCard(card, options);
     },
 
     choosePreferred(existingCard = {}, candidateCard = {}) {
-        const existingPriority = Number(existingCard?.archive_priority ?? this.getPriority(existingCard));
-        const candidatePriority = Number(candidateCard?.archive_priority ?? this.getPriority(candidateCard));
-
-        if (candidatePriority > existingPriority) {
-            return candidateCard;
-        }
-
-        if (candidatePriority < existingPriority) {
-            return existingCard;
-        }
-
-        const existingHasImage = this.hasUsableImage(existingCard);
-        const candidateHasImage = this.hasUsableImage(candidateCard);
-
-        if (candidateHasImage && !existingHasImage) {
-            return candidateCard;
-        }
-
-        if (existingHasImage && !candidateHasImage) {
-            return existingCard;
-        }
-
-        const existingScore = this.getCompletenessScore(existingCard);
-        const candidateScore = this.getCompletenessScore(candidateCard);
-
-        if (candidateScore > existingScore) {
-            return candidateCard;
-        }
-
-        return existingCard;
+        return ArchivePreference.choosePreferred(existingCard, candidateCard);
     },
 
     mergeCards(cards = []) {
@@ -143,19 +52,7 @@ export const ArchiveMerge = {
     },
 
     sortCards(cards = []) {
-        return [...Utils.normalizeArray(cards)].sort((a, b) => {
-            const categoryA = Utils.normalizeString(a?.card_category || a?.type);
-            const categoryB = Utils.normalizeString(b?.card_category || b?.type);
-
-            if (categoryA !== categoryB) {
-                return categoryA.localeCompare(categoryB, 'de');
-            }
-
-            const nameA = Utils.normalizeString(a?.name || a?.id);
-            const nameB = Utils.normalizeString(b?.name || b?.id);
-
-            return nameA.localeCompare(nameB, 'de');
-        });
+        return ArchiveSort.sortCards(cards);
     }
 };
 
