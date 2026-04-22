a5ff8c11cdaf07e2a9192c242697c215b22b90f7
Kharas81 <34625007+Kharas81@users.noreply.github.com>
2026-04-22 09:03:09 +0200

    Create config-catalogs.js

---
+++ js/core/config-catalogs.js
@@ -0,0 +1,14 @@
+const CONFIG_CATALOGS = {
+    schergen: {
+        key: 'schergen',
+        enabled: true,
+        dataDir: 'data/cards/catalog/schergen',
+        imageDir: 'assets/images/cards/schergen',
+        indexFile: 'data/cards/catalog/schergen/index.json',
+        runtimeBundle: 'data/runtime/catalogs/schergen.cards.json',
+        defaultType: 'minion',
+        defaultCardCategory: 'schergenkarte'
+    }
+};
+
+export default CONFIG_CATALOGS;
