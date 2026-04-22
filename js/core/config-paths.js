85752b09775cc25b0ae1016a1f399558a2abd692
Kharas81 <34625007+Kharas81@users.noreply.github.com>
2026-04-22 09:02:45 +0200

    Create config-paths.js

---
+++ js/core/config-paths.js
@@ -0,0 +1,9 @@
+const CONFIG_PATHS = {
+    dataRoot: 'data',
+    adventuresRoot: 'data/adventures',
+    cardsRoot: 'data/cards',
+    manualRoot: 'data/manual',
+    runtimeRoot: 'data/runtime'
+};
+
+export default CONFIG_PATHS;
