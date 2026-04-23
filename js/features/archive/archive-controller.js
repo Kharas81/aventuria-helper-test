073ead8b95b5adfecdc2925b90cf9b2a4977fd1f
Kharas81 <34625007+Kharas81@users.noreply.github.com>
2026-04-23 10:10:42 +0200

    Refactor renderHome method and add buildHomeViewModel

--- js/features/archive/archive-controller.js
+++ js/features/archive/archive-controller.js
@@ -18,21 +18,24 @@ export const ArchiveController = {
         return archiveSets.length ? archiveSets : (CONFIG.getEnabledSets?.() || []);
     },
 
-    renderHome() {
+    buildHomeViewModel() {
         const availableArchiveSets = this.getResolvedArchiveSets();
 
         if (!availableArchiveSets.some(setConfig => setConfig.id === ArchiveState.currentSet)) {
             ArchiveState.currentSet = availableArchiveSets[0]?.id || CONFIG.defaultSet || 'base_game';
         }
 
-        ArchiveState.isHomeView = true;
-
-        ArchiveRenderer.renderHome({
+        return {
             activeSetKey: ArchiveState.currentSet,
             activeSetName: CONFIG.getSetDisplayName?.(ArchiveState.currentSet),
             enabledSets: availableArchiveSets,
             totalLoadedCards: ArchiveState.allCards.length
-        });
+        };
+    },
+
+    renderHome() {
+        ArchiveState.isHomeView = true;
+        ArchiveRenderer.renderHome(this.buildHomeViewModel());
     },
 
     applyFilters() {
